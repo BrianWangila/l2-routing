93c3e1ecb4411fee396e8e30ef421a488bd0676f
Brian Wangila <bwanjalah@gmail.com>
2022-07-20 13:03:45 +0300

    react routing code along

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,10 @@ import Navbar from '../navbar/Navbar';
 import Contact from '../contact/Contact';
 import About from '../about/About';
 import Home from '../home/Home';
+import User from '../user/User'
+import { Doubler } from '../user/User';
+import { MemoryRouter as Router, Routes, Link, Route } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -11,10 +15,41 @@ function App() {
     <Home/>
     <Contact/>
     <About/>
+    <User/>
    </div>
   );
 }
 
+function MainApp(){
 
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const links = numbers.map((number) => (<Link to={`/doubler/${number}`}>ITEM: {number}</Link>))
+
+  return(
+    <Router>
+      {/* All routes go here */}
+      <div className="topNavBar">
+        {/* Links can go anywhere */}
+        <Link className="active" to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+        <Link to="/user/faith">User Faith</Link>
+        <Link to="/doubler/1">DOUBLE</Link>
+        {links}
+
+      </div>
+
+      <Routes>
+        {/* individual route go here */}
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/about" element={<About />} />
+        <Route path="/user/:username" element={<User />} />
+        <Route path="/doubler/:num" element={<Doubler />} />
+      </Routes>
+    </Router>
+  )
+}
 
 export default App;
+export {MainApp}
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function User(){
+
+    let {username} = useParams()
 
     return(
         <div>
             <center style={{marginTop: 5 +'em', fontSize: 30 + 'px'}}>
-                User: 
+                User: {username}
             </center>
         </div>
     )
 
 }
+
+function Doubler(){
+
+    let { num } = useParams()
+    const double = num * 2
+
+    return (
+        <center style={{marginTop: 5 +'em', fontSize: 30 + 'px'}}>
+            Double of {num} is {double}
+        </center>
+    )
+}
+
+export {Doubler}
